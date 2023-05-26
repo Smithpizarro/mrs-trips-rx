02374f2a04563618d64ed2bc2c250182841822db
Jonathan Smith <jpizarroaguado@gmail.com>
2023-05-26 14:53:17 -0500

    refactor : TEST-01 modificate tripsrxApplication

--- src/main/java/pe/business/app/strips/MrsTripsRxApplication.java
+++ src/main/java/pe/business/app/strips/MrsTripsRxApplication.java
@@ -5,12 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
-import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
-import org.springframework.r2dbc.connection.R2dbcTransactionManager;
-import org.springframework.r2dbc.connection.init.CompositeDatabasePopulator;
-import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
-import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
-import org.springframework.transaction.ReactiveTransactionManager;
+
 import org.springframework.web.reactive.config.EnableWebFlux;
 
 import io.r2dbc.spi.ConnectionFactory;
